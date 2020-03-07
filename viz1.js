4cd064b6c74b9678793e51ce821c16370a8e4931
ayman1996 <60525674+ayman1996@users.noreply.github.com>
2020-03-07 09:07:36 +0100

    Update viz1.js

--- viz1.js
+++ viz1.js
@@ -1,11 +1,13 @@
 Plotly.d3.csv('https://raw.githubusercontent.com/ayman1996/Dataismine/master/earthquakes.csv',
-  function(err, rows){function unpack(rows, key) {return rows.map(function(row){ return row[key];
+  function(err, rows){
+  function unpack(rows, key) {return rows.map(function(row){ return row[key];
 })};
 
 var data = [{
   lon: unpack(rows, 'location.longitude'), lat: unpack(rows, 'location.latitude'), radius:10,
   z: unpack(rows, 'impact.magnitude'), type: "densitymapbox", coloraxis: 'coloraxis',
-  hoverinfo: 'location.name'}];
+  hoverinfo: 'location.name'
+}];
 
 var layout = {
     mapbox: {center: {lon: 60, lat: 30}, style: "outdoors", zoom: 2},
